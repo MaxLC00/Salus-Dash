a0b63e0b02896e6b13bb91ce706840f7f4462f3a
Max LC <167907843+MaxLC00@users.noreply.github.com>
2024-11-14 21:07:44 -0600

    render init

---
+++ client/src/pages/Sheets.jsx
@@ -0,0 +1,16 @@
+import Jumbotron from "../components/Jumbotron";
+import Auth from "../utils/auth";
+
+
+const Sheets = () => {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>ABOUT</h1>
+        <p>All about the creators of this project</p>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Sheets;
